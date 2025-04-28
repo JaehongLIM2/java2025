560d465c7142b7d7936d8e4664c8946d96e0cec9
JaehongLIM2 <nu0044@naver.com>
2025-04-28 14:45:55 +0900

    java 실수형

---
+++ src/ch02/ch02/sec04/FloatDoubleExample.java
@@ -0,0 +1,22 @@
+package ch02.ch02.sec04;
+
+public class FloatDoubleExample {
+    public static void main(String[] args) {
+        float var1 = 0.1234567890123456789f;
+        double var2 = 0.1234567890123456789;
+
+        System.out.println("var1 = " + var1);
+        System.out.println("var2 = " + var2);
+
+
+        double var3 = 3e6; // 3.0 * 1000000
+        float var4 = 3e6F;
+        double var5 = 2e-3; // 2.0 *0. 001
+
+        System.out.println("var3 = " + var3);
+        System.out.println("var4 = " + var4);
+        System.out.println("var5 = " + var5);
+
+
+    }
+}
---
+++ src/ch02/lecture/C06RealNumber.java
@@ -0,0 +1,20 @@
+package ch02.lecture;
+
+public class C06RealNumber {
+    public static void main(String[] args) {
+        // 실수 :
+        // float : (4 byte, 16bits)
+        float a = 3.14F;
+        System.out.println("a = " + a);
+
+
+        // double : (8 byte, 64bits)
+        // 더 넓은 범위를 더 촘촘하게 표현함.
+        double b = 3.14;
+        System.out.println("b = " + b);
+
+        // java 코드에서 실수 literal 은 double로 인식
+        // java 에서 실수 literal 이 float 로 표현하려면 끝에 F(f)
+
+    }
+}
