bacb83dda5cc354f358957cbc61ad56369e3b3d9
JaehongLIM2 <nu0044@naver.com>
2025-05-08 14:15:54 +0900

    method overloading 확인문제 16

---
+++ src/ch06/exercise/p16/Printer.java
@@ -0,0 +1,19 @@
+package ch06.exercise.p16;
+
+public class Printer {
+    void println(int values) {
+        System.out.println(values);
+    }
+
+    void println(boolean values) {
+        System.out.println(values);
+    }
+
+    void println(double values) {
+        System.out.println(values);
+    }
+
+    void println(String values) {
+        System.out.println(values);
+    }
+}
