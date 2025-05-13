6c78e8b71b57c66c62caa7bbc063e67f0c432d98
JaehongLIM2 <nu0044@naver.com>
2025-05-13 17:27:37 +0900

    확인문제 3

---
+++ src/leetCode/exercise/Main.java
@@ -0,0 +1,16 @@
+package leetCode.exercise;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("|\\_/|");
+        System.out.println();
+        System.out.print("|q p|   /}");
+        System.out.println();
+        System.out.print("( 0 )\"\"\"\\");
+        System.out.println();
+        System.out.print("|\"^\"`    |");
+        System.out.println();
+        System.out.print("||_/=\\\\__|");
+    }
+}
