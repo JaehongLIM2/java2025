0edb8f05af8dd9b58d7e1a76cf551564e1905136
JaehongLIM2 <nu0044@naver.com>
2025-05-13 14:54:38 +0900

    instanceof

--- src/ch08/lecture/p03cast/App02.java
+++ src/ch08/lecture/p03cast/App02.java
@@ -1,5 +1,22 @@
 package ch08.lecture.p03cast;
 
 public class App02 {
-    
+    public static void main(String[] args) {
+        MyClass021 a = new MyClass021();
+        MyInterface02 b = a;
+        MyClass021 c = (MyClass021) b; // ok
+        MyClass022 d = (MyClass022) b; // ClassCastException
+
+
+        System.out.println("안전! 안전! 안전!");
+    }
+}
+
+interface MyInterface02 {
+}
+
+class MyClass021 implements MyInterface02 {
+}
+
+class MyClass022 implements MyInterface02 {
 }
