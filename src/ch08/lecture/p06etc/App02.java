796639081007ac2b43a75430c3347532a5616a77
JaehongLIM2 <nu0044@naver.com>
2025-05-14 09:41:21 +0900

    인터페이스의 static 메소드

--- src/ch08/lecture/p06etc/App02.java
+++ src/ch08/lecture/p06etc/App02.java
@@ -2,3 +2,40 @@ package ch08.lecture.p06etc;
 
 public class App02 {
 }
+
+interface Myinterface02 {
+    void action();
+
+    // default method : 인터페이스에 있는 body 가 있는 인스턴스 메소드(추상메소드가 아님)
+    default void method1() {
+        System.out.println("Myinterface02.method1");
+    }
+
+    ;
+
+    // default 메소드도 항상 public
+    public default void hello() {
+        System.out.println("Myinterface02.hello");
+    }
+
+}
+
+class MyClass021 implements Myinterface02 {
+    @Override
+    public void action() {
+        System.out.println("MyClass021.action");
+    }
+    // 필요하면 default 메소드도 재정의 가능
+
+    @Override
+    public void method1() {
+        System.out.println("MyClass021.method1");
+    }
+}
+
+class MyClass022 implements Myinterface02 {
+    @Override
+    public void action() {
+        System.out.println("MyClass022.action");
+    }
+}
---
+++ src/ch08/lecture/p06etc/App03.java
@@ -0,0 +1,21 @@
+package ch08.lecture.p06etc;
+
+public class App03 {
+    public static void main(String[] args) {
+        MyInterface03.action();
+    }
+}
+
+interface MyInterface03 {
+    // public static final field
+
+    // public static method
+    static void action() {
+        System.out.println("MyInterface03.action");
+    }
+
+    // public abstract method(instance method)
+
+    // public default method(instance method)
+
+}
