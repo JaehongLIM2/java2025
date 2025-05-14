23a13778ab8eb3348c0f9066959cbb4fcfac5b4e
JaehongLIM2 <nu0044@naver.com>
2025-05-14 15:43:51 +0900

    lambda 확인문제까지

---
+++ src/ch16/lecture/p01/lambda/App04.java
@@ -0,0 +1,22 @@
+package ch16.lecture.p01.lambda;
+
+public class App04 {
+    public static void main(String[] args) {
+        MyInterface04 a1 = (int a, String b) -> {
+            System.out.println(b + ", " + a);
+        };
+
+
+        MyInterface04 a2 = (a, b) -> {
+            System.out.println(b + ", " + a);
+        };
+
+        MyInterface04 c = (x, y) -> System.out.println(y + ", " + x);
+
+    }
+}
+
+@FunctionalInterface
+interface MyInterface04 {
+    void action(int a, String b);
+}
---
+++ src/ch16/lecture/p01/lambda/App05.java
@@ -0,0 +1,23 @@
+package ch16.lecture.p01.lambda;
+
+public class App05 {
+    public static void main(String[] args) {
+        MyInterface05 o = (a) -> {
+            System.out.println(a + "이다");
+            System.out.println(a + "입니다.");
+        };
+
+        MyInterface05 z = a -> {
+            System.out.println(a + "이다");
+            System.out.println(a + "입니다.");
+        };
+
+        MyInterface05 w = a -> System.out.println(a + "이다");
+
+    }
+}
+
+@FunctionalInterface
+interface MyInterface05 {
+    void method(int a);
+}
