e4d1f58eb302b4080d8b8c748fe91577dc984541
JaehongLIM2 <nu0044@naver.com>
2025-05-15 20:01:15 +0900

    확인문제 6

---
+++ src/ch12/exercise/p06/MemberExample.java
@@ -0,0 +1,10 @@
+package ch12.exercise.p06;
+
+public class MemberExample {
+    public static void main(String[] args) {
+        Member member = new Member("blue", "이파란");
+        Member member1 = new Member("red", "김빨강");
+        System.out.println(member);
+        System.out.println(member1);
+    }
+}
