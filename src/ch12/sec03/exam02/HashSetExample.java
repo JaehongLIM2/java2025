acfc9cc7d192670fa4d8d4aa877f9d5c7e2d5f0c
JaehongLIM2 <nu0044@naver.com>
2025-05-15 11:56:37 +0900

    hashcode

---
+++ src/ch12/sec03/exam02/HashSetExample.java
@@ -0,0 +1,11 @@
+package ch12.sec03.exam02;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        HashSet hashSet = new HashSet();
+
+        Student s1 = new Student();
+    }
+}
