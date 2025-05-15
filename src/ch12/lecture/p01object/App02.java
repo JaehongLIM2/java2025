2d5e63dda15590a259519abfbd03ec3821fd1282
JaehongLIM2 <nu0044@naver.com>
2025-05-15 11:29:30 +0900

    equals

--- src/ch12/lecture/p01object/App02.java
+++ src/ch12/lecture/p01object/App02.java
@@ -1,5 +1,35 @@
 package ch12.lecture.p01object;
 
+import ch07.exercise.p12.B;
+
 public class App02 {
-    
+    public static void main(String[] args) {
+        Book02 b1 = new Book02();
+        Book02 b2 = new Book02();
+        Book02 b3 = new Book02();
+
+
+        b1.setTitle("이것이 자바다");
+        b2.setTitle("pro git");
+        b3.setTitle("이것이 자바다");
+
+        // 참조값 비교
+        System.out.println(b1 == b2); // false
+
+        // equals : 내용이 같은가
+        System.out.println(b1.equals(b2)); // false
+        System.out.println(b1.equals(b3)); // false
+    }
+}
+
+class Book02 {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
