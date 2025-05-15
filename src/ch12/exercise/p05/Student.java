9b4f1a19c91ee8e5c73635dbac817e52e6836d58
JaehongLIM2 <nu0044@naver.com>
2025-05-15 12:32:46 +0900

    집에서ㅗ함

---
+++ src/ch12/exercise/p05/Student.java
@@ -0,0 +1,23 @@
+package ch12.exercise.p05;
+
+import java.util.Objects;
+
+public class Student {
+    private String StudentNum;
+
+    public Student(String studentNum) {
+        this.StudentNum = studentNum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(StudentNum, student.StudentNum);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(StudentNum);
+    }
+}
--- src/ch12/sec03/exam02/HashSetExample.java
+++ src/ch12/sec03/exam02/HashSetExample.java
@@ -6,6 +6,9 @@ public class HashSetExample {
     public static void main(String[] args) {
         HashSet hashSet = new HashSet();
 
-        Student s1 = new Student();
+        Student s1 = new Student(1, "홍길동");
+        hashSet.add(s1);
+
+
     }
 }
