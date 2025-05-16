ec3ec8134f7224cdd6189d976a2be23e9a95bb22
JaehongLIM2 <nu0044@naver.com>
2025-05-16 10:13:15 +0900

    Pattern.matches

--- src/ch12/practice/App02.java
+++ src/ch12/practice/App02.java
@@ -7,20 +7,20 @@ public class App02 {
     public static void main(String[] args) {
         // 사용자로부터 입력받아서
         Scanner scanner = new Scanner(System.in);
-
+        String pattern = "[가-힣]{3,}";
 
         // 한글 3글자 이상인지 확인하는 코드 작성
 
         while (true) {
             System.out.print("입력 : ");
             String userInput = scanner.nextLine();
-            boolean run = Pattern.matches("[가-힣]{3,}", userInput);
+            boolean run = Pattern.matches(pattern, userInput);
 
 
             if (run) {
                 System.out.println("한글 3글자 이상 입력하셨습니다.");
             } else {
-                System.out.println("한글 3글자 이상 입력하지않으셨습니다.");
+                System.out.println("한글 3글자 이상 입력하지 않으셨습니다.");
             }
         }
 
