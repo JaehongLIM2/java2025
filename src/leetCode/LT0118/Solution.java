27af8c05b371dec636917076fa0312ef56e0967a
JaehongLIM2 <nu0044@naver.com>
2025-05-19 17:01:26 +0900

    leetCode 0118번

---
+++ src/leetCode/LT0118/Solution.java
@@ -0,0 +1,27 @@
+package leetCode.LT0118;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1); // 0번째 행
+
+            if (i > 0) {
+                List<Integer> prevRow = result.get(i - 1);
+                for (int j = 1; j < i; j++) {
+                    int v = prevRow.get(j - 1) + prevRow.get(j);
+                    row.add(v);
+                }
+                if (i > 0) {
+                    row.add(1);
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
