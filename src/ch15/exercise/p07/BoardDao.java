4b7bc3edf9a4a3e1f343a11e45d05d142353a164
JaehongLIM2 <nu0044@naver.com>
2025-05-19 12:38:36 +0900

    확인문제 7 복습

--- src/ch15/exercise/p07/BoardDao.java
+++ src/ch15/exercise/p07/BoardDao.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BoardDao {
+
     public List<Board> getBoardList() {
         List<Board> list = new ArrayList<>();
-        list.add(new Board("제목1", "내용1"));
-        list.add(new Board("제목2", "내용2"));
-        list.add(new Board("제목3", "내용3"));
-        return list;
 
+        list.add(new Board("제목 1", "내용 1"));
+        list.add(new Board("제목 2", "내용 2"));
+        list.add(new Board("제목 3", "내용 3"));
+
+
+        return list;
     }
 }
