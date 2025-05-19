633e725cf72b1d6ea308b458c81e96db570d02ea
JaehongLIM2 <nu0044@naver.com>
2025-05-19 11:16:19 +0900

    리트코드 1431

---
+++ src/leetCode/LT1431/Solution.java
@@ -0,0 +1,43 @@
+package leetCode.LT1431;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+//        List<Integer> kids = Arrays.stream(candies).boxed().toList();
+
+        candies = new int[]{2, 3, 5, 1, 3};
+        extraCandies = 3;
+        // 1. kids List에서 가장 큰 값 구하기
+        int max = candies[0];
+        for (int i = 1; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+
+        // 2. List<Boolean> 객체 만들기
+        List<Boolean> list = new ArrayList<>();
+
+
+        // 3. kids의 각 요소에 extraCandy 더해서 (1에서 구한 가장 큰값)보다 크거나 같으면
+        // result 에 true 추가
+        // 아니면 false 추가
+
+        for (int i = 0; i < candies.length; i++) {
+            if ((candies[i] + extraCandies) > max) {
+                list.add(true);
+            } else {
+                list.add(false);
+            }
+        }
+
+
+        // result 리턴
+
+        return list;
+    }
+}
