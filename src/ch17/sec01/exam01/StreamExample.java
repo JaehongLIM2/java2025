f4add2659d51b5aee0817978bfad58fb7048e423
JaehongLIM2 <nu0044@naver.com>
2025-05-21 15:10:29 +0900

    stream 최종연산

---
+++ src/ch17/sec01/exam01/StreamExample.java
@@ -0,0 +1,17 @@
+package ch17.sec01.exam01;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StreamExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("홍길동");
+        set.add("신용권");
+        set.add("감자바");
+
+        set.stream()
+                .forEach(System.out::println);
+        
+    }
+}
