810a178a8ae99688d106d2875f7bce3b6c875558
JaehongLIM2 <nu0044@naver.com>
2025-05-21 14:30:58 +0900

    stream 사용법( 중간연산, 최종연산 method chaining)

---
+++ src/ch17/lecture/p01stream/App04.java
@@ -0,0 +1,36 @@
+package ch17.lecture.p01stream;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class App04 {
+    public static void main(String[] args) {
+        // collection(또는 배열)에서 stream 만들고
+        // 연산(메소드) 실행
+
+        List<Integer> list = List.of(6, 7, 8, 9, 8, 12, 3, 1);
+
+
+        // 1. 중간 연산 (intermediate operation)
+        //     * 리턴타입 : Stream(...stream)
+        //     * 최종 연산이 없으면 실행되지 않음
+        Stream<Integer> s1 = list.stream(); // 만들기
+        Stream<Integer> s2 = s1.filter(i -> i % 2 == 0); // 중간연산
+        Stream<Integer> s3 = s2.map(i -> i * i); // 중간연산
+
+
+        // 2. 최종 연산 (terminal operation)
+        //     * 리턴타입 : Stream이 아님
+        s3.forEach(System.out::println);
+
+        System.out.println("-----");
+        // method chaining
+        list.stream()
+                .filter(i -> i % 2 == 1)
+                .map(i -> i * i)
+                .forEach(System.out::println);
+
+
+        System.out.println("프로그램 종료");
+    }
+}
