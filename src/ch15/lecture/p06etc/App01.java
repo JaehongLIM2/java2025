b099c75c5931f07777f8a252d9255da446662b26
JaehongLIM2 <nu0044@naver.com>
2025-05-21 09:17:09 +0900

    linked list

---
+++ src/ch15/lecture/p06etc/App01.java
@@ -0,0 +1,18 @@
+package ch15.lecture.p06etc;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class App01 {
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new LinkedList<>();
+
+
+        // ArrayList : 내부에 array 사용
+        // LinkedList : 내부에 doubly linked list (양쪽으로 연결된 객체들) 사용
+
+
+    }
+}
