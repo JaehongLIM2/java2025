8115a31560c2f57d02dad1233b38b717cee33493
JaehongLIM2 <nu0044@naver.com>
2025-05-21 16:27:09 +0900

    Stream

--- src/ch17/sec07/exam01/SortingExample.java
+++ src/ch17/sec07/exam01/SortingExample.java
@@ -2,18 +2,28 @@ package ch17.sec07.exam01;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortingExample {
     public static void main(String[] args) {
-        List<Student> list = new ArrayList<>();
-        list.add(new Student("홍길동", 30));
-        list.add(new Student("신용권", 10));
-        list.add(new Student("유미선", 20));
+        List<Student> list = List.of(
+                new Student("홍길동", 30),
+                new Student("신용권", 10),
+                new Student("유미선", 20)
+        );
 
         list.stream()
                 .sorted()
-                .forEach(x -> System.out.println(x.getName() + " : " + x.getScore()));
+                .forEach(System.out::println);
+        System.out.println();
+        list.stream()
+                .sorted((s1, s2) -> s2.compareTo(s1))
+                .forEach(System.out::println);
+        System.out.println();
+        list.stream()
+                .sorted(Comparator.reverseOrder())
+                .forEach(System.out::println);
 
 
         // 명령형
