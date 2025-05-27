afcd798bad546b6f472be83544c579fe294ebc97
JaehongLIM2 <nu0044@naver.com>
2025-05-27 14:44:26 +0900

    thread

--- src/ch14/sec03/exam02/BeepPrintExample.java
+++ src/ch14/sec03/exam02/BeepPrintExample.java
@@ -1,32 +1,33 @@
-package ch14.lecture;
+package ch14.sec03.exam02;
 
 import java.awt.*;
 
-public class App02 {
-    public static void main(String[] args) throws InterruptedException {
+public class BeepPrintExample {
+    public static void main(String[] args) {
         Thread thread = new Thread(() -> {
             Toolkit toolkit = Toolkit.getDefaultToolkit();
-
             for (int i = 0; i < 5; i++) {
                 toolkit.beep();
-
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-
         });
 
         thread.start();
 
         for (int i = 0; i < 5; i++) {
-            System.out.println("띠리링");
+            System.out.println("띵");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
 
-            Thread.sleep(1000);
         }
 
-
     }
-}
+
