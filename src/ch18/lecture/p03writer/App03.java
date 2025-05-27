500a0281853e6fc3b663f81f38867946d1365bbd
JaehongLIM2 <nu0044@naver.com>
2025-05-27 19:31:38 +0900

    writer

---
+++ src/ch18/lecture/p03writer/App03.java
@@ -0,0 +1,23 @@
+package ch18.lecture.p03writer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class App03 {
+    public static void main(String[] args) {
+        try (Writer writer = new FileWriter("C:/Temp/writer03.txt")) {
+            writer.write('a');
+            writer.write('한');
+            writer.write('\u2669');
+//            writer.write('\u1f9e2');
+            writer.write("💕");
+
+
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
