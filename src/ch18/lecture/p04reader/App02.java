5338076ddff2936b4c9a3515fd123885f8b2abe8
JaehongLIM2 <nu0044@naver.com>
2025-05-27 19:54:16 +0900

    reader

---
+++ src/ch18/lecture/p04reader/App02.java
@@ -0,0 +1,28 @@
+package ch18.lecture.p04reader;
+
+import java.io.*;
+
+public class App02 {
+    public static void main(String[] args) throws IOException {
+
+        // reader 연습용 파일 만들기
+        String fileName = "C:/Temp/reader02.txt";
+        try (Writer writer = new FileWriter(fileName)) {
+            writer.write('a');
+            writer.write('한');
+            writer.write('글');
+            writer.write("날");
+            writer.write("🧢");
+            writer.flush();
+        }
+
+        // reader 연습
+        try (Reader reader = new FileReader(fileName)) {
+            char[] data = new char[10];
+            int length = reader.read(data);
+
+            System.out.println("length = " + length);
+        }
+
+    }
+}
