cea722dd92e1304a06dc68e476e9cfe020cd11cb
JaehongLIM2 <nu0044@naver.com>
2025-05-27 18:23:45 +0900

    inputstream

---
+++ src/ch18/lecture/p02input/App03.java
@@ -0,0 +1,45 @@
+package ch18.lecture.p02input;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class App03 {
+    public static void main(String[] args) throws IOException {
+
+        // inputstream 에서 사용할 파일을 먼저 만들기
+        String fileName = "C:/Temp/input03.data";
+        try (OutputStream os = new FileOutputStream(fileName)) {
+            for (int i = 0; i < 35; i++) {
+                os.write(i);
+
+            }
+        }
+        System.out.println("파일 만들기 종료");
+
+
+        // inputstream 으로 읽기
+        // 35 bytes 파일 크기
+        byte[] datas;
+        try (InputStream is = new FileInputStream(fileName)) {
+
+            datas = new byte[10];
+            int r1 = is.read(datas);// 몇 byte 읽었는지 return
+            System.out.println("Arrays.toString(datas) = " + Arrays.toString(datas));
+            int r2 = is.read(datas);
+            System.out.println("Arrays.toString(datas) = " + Arrays.toString(datas));
+            int r3 = is.read(datas);
+            System.out.println("Arrays.toString(datas) = " + Arrays.toString(datas));
+            int r4 = is.read(datas);
+            System.out.println("Arrays.toString(datas) = " + Arrays.toString(datas));
+            int r5 = is.read(datas);
+            System.out.println("Arrays.toString(datas) = " + Arrays.toString(datas));
+
+            System.out.println("r1 = " + r1);
+            System.out.println("r2 = " + r2);
+            System.out.println("r3 = " + r3);
+            System.out.println("r4 = " + r4);
+            System.out.println("r5 = " + r5);
+
+        }
+    }
+}
--- src/ch18/sec02/exam01/WriteExample.java
+++ src/ch18/sec02/exam01/WriteExample.java
@@ -7,19 +7,19 @@ import java.io.OutputStream;
 public class WriteExample {
     public static void main(String[] args) throws IOException {
         try {
-            OutputStream os = new FileOutputStream("C:/Temp/test1.db");
+            try (OutputStream os = new FileOutputStream("C:/Temp/test1.db")) {
 
 
-            byte a = 10;
-            byte b = 20;
-            byte c = 30;
+                byte a = 10;
+                byte b = 20;
+                byte c = 30;
 
-            os.write(a);
-            os.write(b);
-            os.write(c);
+                os.write(a);
+                os.write(b);
+                os.write(c);
 
-            os.flush();
-            os.close();
+                os.flush();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
