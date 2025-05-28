0fb44959b341939270732b8a8406392f0b1eabf0
JaehongLIM2 <nu0044@naver.com>
2025-05-28 11:47:34 +0900

    annotation element

--- src/ch12/sec12/PrintAnnotationExample.java
+++ src/ch12/sec12/PrintAnnotationExample.java
@@ -1,30 +1,28 @@
 package ch12.sec12;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class PrintAnnotationExample {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+
         Method[] methods = Service.class.getDeclaredMethods();
-        for (Method method : methods) {
-            PrintAnnotation printAnnotation = method.getAnnotation(PrintAnnotation.class);
 
+        for (var method : methods) {
+            System.out.println(method.getName());
+            var printAnnotation = method.getAnnotation(PrintAnnotation.class);
             printLine(printAnnotation);
 
-            method.invoke(new Service());
-
-            printLine(printAnnotation);
         }
     }
 
-    private static void printLine(PrintAnnotation printAnnotation) {
-        if (printAnnotation != null) {
-            int number = printAnnotation.number();
-            for (int i = 0; i < number; i++) {
-                String value = printAnnotation.value();
-                System.out.println(value);
-            }
-            System.out.println();
+    public static void printLine(PrintAnnotation printAnnotation) {
+        int number = printAnnotation.number();
+
+        for (int i = 0; i < number; i++) {
+            System.out.print(printAnnotation.value());
         }
+        System.out.println();
+
     }
 }
+
--- src/ch12/sec12/Service.java
+++ src/ch12/sec12/Service.java
@@ -1,18 +1,20 @@
 package ch12.sec12;
 
 public class Service {
-    @PrintAnnotation
+
+    @PrintAnnotation()
     public void method1() {
-        System.out.println("실행내용 1");
+        System.out.println("Service.method1");
     }
 
-    @PrintAnnotation
+    @PrintAnnotation("*")
     public void method2() {
-        System.out.println("실행내용 2");
+        System.out.println("Service.method2");
     }
 
-    @PrintAnnotation
+    @PrintAnnotation(value = "#", number = 20)
     public void method3() {
-        System.out.println("실행내용 3");
+        System.out.println("Service.method3");
     }
+
 }
