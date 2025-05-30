df02ce665c28aba6df616bb55bb00900ac93ee6a
JaehongLIM2 <nu0044@naver.com>
2025-05-30 17:47:47 +0900

    Server, Client 연결

---
+++ src/ch19/lecture/App02Client.java
@@ -0,0 +1,6 @@
+package ch19.lecture;
+
+public class App02Client {
+    // client : server 에 연결 시도
+
+}
---
+++ src/ch19/lecture/App02Server.java
@@ -0,0 +1,8 @@
+package ch19.lecture;
+
+import java.net.ServerSocket;
+
+public class App02Server {
+    // server : client 기다림
+    ServerSocket serverSocket = new ServerSocket();
+}
